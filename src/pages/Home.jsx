0eeebc6c912970616da90ae2ce12db638b5ad2ed
chrisaharaldsen94 <chris.a.haraldsen@hiof.no>
2026-02-27 14:15:08 +0100

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,42 @@
+import { useState } from "react"
+
 export default function Home(){
-    return <h1>Forside</h1>
+    const [search, setSearch] = useState('')
+
+    const baseUrl = `http://www.omdbapi.com/?s=${search}&apikey=`
+
+    //Ikke gjør sånn!!
+    const apiKey = "....."
+
+
+    const getMovies = async()=>{
+        try
+        {
+            const response = await fetch(`${baseUrl}&t=james-bond`)
+            const data = await response.json()
+            
+            console.log(data)
+
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+
+    const handleChange = (e)=>{
+        setSearch(e.target.value)
+    }
+
+    return(
+    <main>
+         <h1>Forside</h1>
+         <form>
+            <label>
+                Søk etter film
+            <input type="search" placeholder="Harry Potter" onChange={handleChange}></input>
+            <label/>
+        </form>
+         {<button onClick={getMovies}>Søk</button>}
+    </main>
+    )
 }
